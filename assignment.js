9ebc8fc32b0244dcb8bafa6f1855739e48e4e75a
thelenaza <kaxtie07@gmail.com>
2022-11-02 13:36:56 +0700

    first commit

---
+++ assignment.js
@@ -0,0 +1,26 @@
+let x = 20;
+console.log(x);
+
+x += 10;
+console.log(x) ; // x = x + 10
+
+let y = 20;
+y -= 10;
+console.log(y); // y = y - 10
+
+let a = 20;
+a *= 10 ;
+console.log(a); // a = a * 10
+
+let b = 20;
+b /= 10;
+console.log(b); // b = b / 10
+
+let c = 20
+c %= 10;
+console.log(c) // c = c % 10 
+
+let d = 20
+d **= 10
+console.log(d); // d = d ** 10 ; d = 20 ** 10
+
